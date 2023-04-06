9d5f5b0dfaeb0a0c8581645715e46d53723f1a76
k.dubov <example@example.com>
2023-04-06 21:49:29 +0300

    Add header mobile

---
+++ src/_data/header.js
@@ -0,0 +1,23 @@
+module.exports = {
+    "title": "JENNA.VENTER@gmail.com",
+    "header": { "title": "my tickets", "link": "" },
+    "list": [
+        {
+            "title": "Create Event",
+            "link": ""
+        },
+        {
+            "title": "saved events",
+            "link": ""
+        },
+        {
+            "title": "EARN COmission",
+            "link": ""
+        },
+        {
+            "title": "My Account",
+            "link": ""
+        }
+    ],
+    "footer": { "title": "Log Out", "link": "" }
+}
