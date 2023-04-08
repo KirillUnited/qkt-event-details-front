1b7618b0aa81d4241bc488bfb1ad6fb040a2e6ea
k.dubov <example@example.com>
2023-04-08 11:48:54 +0300

    Set sticky panels positions

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,22 +1,26 @@
 (function () {
     const header = document.getElementById("header");
-    const tabsContainer = document.querySelector(".tabs");
-    const tabslist = tabsContainer.querySelector(".tabs-list");
-    const tabs = tabslist.querySelectorAll(".tabs-link");
+    const tabsContainer = document.querySelectorAll(".tabs");
+    const handleTabs = (element) => {
+        const tabslist = element.querySelector(".tabs-list");
+        const tabs = tabslist.querySelectorAll(".tabs-link");
+
+        tabs.forEach((tab) => {
+            tab.addEventListener("click", onTabClick);
+        });
+
+        function onTabClick(e) {
+            const currentTab = tabslist.querySelector('.tabs-link-active');
+
+            if (e.currentTarget !== currentTab) {
+                switchTab(currentTab, e.currentTarget);
+            }
+        }
+    };
+
+    tabsContainer.forEach((element) => handleTabs(element));
 
     document.addEventListener("scroll", onScroll);
-
-    tabs.forEach((tab) => {
-        tab.addEventListener("click", onTabClick);
-    });
-
-    function onTabClick(e) {
-        const currentTab = tabslist.querySelector('.tabs-link-active');
-
-        if (e.currentTarget !== currentTab) {
-            switchTab(currentTab, e.currentTarget);
-        }
-    }
 
     function switchTab(oldTab, newTab) {
         newTab.focus();
