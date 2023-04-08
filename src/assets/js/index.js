109534c9e443ec28e1baac2ca2f7f66403459ec9
k.dubov <example@example.com>
2023-04-08 10:50:39 +0300

    Add active tab toggle

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,3 +1,28 @@
-document.addEventListener("DOMContentLoaded", () => {
-    
-});
+(function () {
+    const header = document.getElementById("header");
+    const tabsContainer = document.querySelector(".tabs");
+    const tabslist = tabsContainer.querySelector(".tabs-list");
+    const tabs = tabslist.querySelectorAll(".tabs-link");
+
+    document.addEventListener("scroll", onScroll);
+
+    tabs.forEach((tab) => {
+        tab.addEventListener("click", onTabClick);
+    });
+
+    function onTabClick(e) {
+        const currentTab = tabslist.querySelector('.tabs-link-active');
+
+        if (e.currentTarget !== currentTab) {
+            switchTab(currentTab, e.currentTarget);
+        }
+    }
+
+    function switchTab(oldTab, newTab) {
+        newTab.focus();
+        newTab.classList.add('tabs-link-active');
+        oldTab.classList.remove('tabs-link-active');
+    }
+
+    function onScroll(e) { }
+})();
