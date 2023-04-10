b6cea26039304f67acae87c886c840caf967d1a4
k.dubov <example@example.com>
2023-04-10 09:46:25 +0300

    [mobile] change active tab on scroll

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -5,35 +5,6 @@
     const handleTabs = (element) => {
         const tabslist = element.querySelector(".tabs-list");
         const tabs = tabslist.querySelectorAll(".tabs-link");
-
-        tabs.forEach((tab) => {
-            tab.addEventListener("click", onTabClick);
-        });
-
-        function onTabClick(e) {
-            const currentTab = tabslist.querySelector('.tabs-link-active');
-
-            if (e.currentTarget !== currentTab) {
-                switchTab(currentTab, e.currentTarget);
-            }
-        }
-    };
-
-    tabsContainer.forEach((element) => handleTabs(element));
-
-    document.addEventListener("scroll", onScroll);
-
-    function switchTab(oldTab, newTab) {
-        if(!oldTab && !newTab) return;
-
-        newTab.classList.add('tabs-link-active');
-        oldTab.classList.remove('tabs-link-active');
-    }
-
-    function onScroll(e) {
-        const tabsContainer = document.querySelector(".tabs");
-        const tabslist = tabsContainer.querySelector(".tabs-list");
-        const tabs = tabslist.querySelectorAll(".tabs-link");
         let current = null;
 
         tabsPanel.forEach((section) => {
@@ -47,11 +18,21 @@
         tabs.forEach((tab) => {
             const newTab = tabslist.querySelector(`[href="#${current}"]`);
 
-            tab.classList.remove('tabs-link-active');
-
-            if(newTab){
-                newTab.classList.add('tabs-link-active');
-            }
+            switchTab(tab, newTab);
         });
+    };
+
+    document.addEventListener("scroll", onScroll);
+
+    function onScroll(e) {
+        tabsContainer.forEach((element) => handleTabs(element));
+    }
+
+    function switchTab(oldTab, newTab) {
+        oldTab.classList.remove('tabs-link-active');
+
+        if (newTab) {
+            newTab.classList.add('tabs-link-active');
+        }
     }
 })();
