09a48ba205c4551b983072249f257c52dfced369
k.dubov <example@example.com>
2023-04-12 11:53:53 +0300

    Implement modal open

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -3,9 +3,12 @@ import "../styles/qkt-events.scss";
 import dropdown from "./components/dropdown";
 import header from "./components/header";
 import tabs from "./components/tabs";
+import modal from "./components/modal";
 // Tabs
 tabs();
 // Dropdown
 dropdown();
 // Header
 header();
+// Modal
+modal();
