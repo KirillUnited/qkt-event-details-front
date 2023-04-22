9da136c926bfabaa3ffca1f4cc013dfe054668ae
k.dubov <example@example.com>
2023-04-22 15:36:10 +0300

    Add header mobile menu

--- src/assets/js/components/header/index.js
+++ src/assets/js/components/header/index.js
@@ -1,5 +1,6 @@
 const header = () => {
     const header = document.getElementById("header");
+    const sideNav = header?.querySelector('.header-sidenav');
     const contentHeading = document.querySelector(".content-heading");
     const headerHandler = () => {
         if (scrollY >= contentHeading.offsetTop - 120) {
@@ -10,10 +11,19 @@ const header = () => {
     };
 
     document.addEventListener("scroll", onScroll);
+    header?.addEventListener('click', onClick);
 
     function onScroll(e) {
         contentHeading && headerHandler();
     }
-}
+
+    function onClick(e) {
+        const menuBtn = e.target.closest('.header-menu-btn');
+
+        if (menuBtn || (e.target === sideNav)) {
+            sideNav?.classList.toggle('isOpen');
+        }
+    }
+};
 
 export default header;
