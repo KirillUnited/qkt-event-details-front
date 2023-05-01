3e03eb7c1c1cf382e65297c9c002a5be300fca41
k.dubov <example@example.com>
2023-05-01 11:32:08 +0300

    Refact header: update config, onScroll

--- src/assets/js/components/header/index.js
+++ src/assets/js/components/header/index.js
@@ -2,29 +2,37 @@ const header = (config = {}) => {
     const model = {
         elementId: "header",
         sideNavSelector: '[data-element="sidenav"]',
+        menuBtnSelector: '[data-element="menuBtn"]',
         contentHeadingSelector: "[data-content-heading]"
     };
-    const {elementId, sideNavSelector, contentHeadingSelector} = Object.assign(model, config);
+    const {
+        elementId,
+        sideNavSelector,
+        menuBtnSelector,
+        contentHeadingSelector
+    } = Object.assign(model, config);
     const element = document.getElementById(elementId);
     const sideNav = element?.querySelector(sideNavSelector);
     const contentHeading = document.querySelector(contentHeadingSelector);
-    const headerHandler = () => {
-        if (scrollY >= contentHeading.offsetTop - 120) {
-            element?.classList.add("isScroll");
-        } else {
-            element?.classList.remove("isScroll");
-        }
-    };
 
     document.addEventListener("scroll", onScroll);
     element?.addEventListener('click', onClick);
 
     function onScroll(e) {
-        contentHeading && headerHandler();
+        if (!contentHeading) {
+            document.removeEventListener("scroll", onScroll);
+            return;
+        }
+
+        if (scrollY >= contentHeading.offsetTop - 120) {
+            element?.classList.add("isScroll");
+        } else {
+            element?.classList.remove("isScroll");
+        }
     }
 
     function onClick(e) {
-        const menuBtn = e.target.closest('[data-element="menuBtn"]');
+        const menuBtn = e.target.closest(menuBtnSelector);
 
         if (menuBtn || (e.target === sideNav)) {
             sideNav.classList.toggle('isOpen');
