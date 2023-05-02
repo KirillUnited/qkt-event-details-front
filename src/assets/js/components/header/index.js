9626175d78f4ba54007a57c5bb7efed0aa64b4be
k.dubov <example@example.com>
2023-05-02 14:16:34 +0300

    Refact header

--- src/assets/js/components/header/index.js
+++ src/assets/js/components/header/index.js
@@ -1,16 +1,10 @@
 const header = (config = {}) => {
-    const model = {
-        elementId: "header",
-        sideNavSelector: '[data-element="sidenav"]',
-        menuBtnSelector: '[data-element="menuBtn"]',
-        contentHeadingSelector: "[data-content-heading]"
-    };
     const {
-        elementId,
-        sideNavSelector,
-        menuBtnSelector,
-        contentHeadingSelector
-    } = Object.assign(model, config);
+        elementId = "header",
+        sideNavSelector = '[data-element="sidenav"]',
+        menuBtnSelector = '[data-element="menuBtn"]',
+        contentHeadingSelector = "[data-content-heading]"
+    } = Object.assign({}, config);
     const element = document.getElementById(elementId);
     const sideNav = element?.querySelector(sideNavSelector);
     const contentHeading = document.querySelector(contentHeadingSelector);
